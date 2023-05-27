13cb6e453097e519cba03591b672c94963f47f31
m1ria <chzaaow@gmail.com>
2023-05-27 08:23:05 +0800

    feat: API统一管理

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>SVG测试</h1>
+    <svg style="width:30px;height:30px;">
+      <use xlink:href="#icon-phone" fill="red"></use>
+    </svg>
+  </div>  
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from '@/App.vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+
+import gloablComponent from './components/index';
+
+import axios from 'axios'
+axios({
+    url: '/api/user/login',
+    method: "post",
+    data: {
+        username: 'admin',
+        passwird: '111111'
+    }
+})
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.use(createPinia())
+
+
+app.use(gloablComponent);
+app.mount('#app')
